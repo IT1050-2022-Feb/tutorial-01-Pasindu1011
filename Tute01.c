996794be4e876ddf8750a19bf75679465392bdc2
it21384738 Ranasinghe <92198044+Pasindu1011@users.noreply.github.com>
2022-03-01 03:28:57 +0000

    change into average calculator

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello world");
+   int mark1,mark2,avarage;
+  printf("Enter mark1: ");
+  scanf("%d",&mark1);
+  printf("Enter mark2: ");
+  scanf("%d",&mark2);
+  avarage = (mark1 + mark2) / 2;
+  printf("Average is: %d",avarage);
+  
   return 0;
 }
 
